429d2bd20a1ea7a0285c2bf315c5907c77d31864
jjkst <karthi.jayaraman@gmail.com>
2026-05-05 08:10:23 -0400

    update api route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiBaseUrl: '/api',
+    apiBaseUrl: 'https://jk-dev.site/api',
     googleClientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
     githubClientId: 'YOUR_GITHUB_CLIENT_ID',
     githubRedirectUri: 'https://jk-dev.site/login'
