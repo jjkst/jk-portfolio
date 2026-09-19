7c0cc7358124b9eb5e9245052e042c85af879427
jjkst <karthi.jayaraman@gmail.com>
2026-09-18 22:25:15 -0400

    switch to new domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://jk-dev.site/api',
+    apiBaseUrl: 'https://api.jk-devsite.com/api',
     googleClientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
     githubClientId: 'YOUR_GITHUB_CLIENT_ID',
-    githubRedirectUri: 'https://jk-dev.site/login'
+    githubRedirectUri: 'https://jk-devsite.com/login'
   };
